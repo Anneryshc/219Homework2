986be4bb08ef22fc043483bab7b7c3b25ca43745
Anneryshc <afh9@njit.edu>
2024-02-05 14:31:57 -0500

    add files

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,2 +1,9 @@
-def add(a,b):
-    return a + b
+"""This is a module containing calculation functions."""
+
+def add(argument_a, argument_b):
+    """This function returns the sum of two numbers."""
+    return argument_a + argument_b
+
+def subtract(argument_a, argument_b):
+    """This function returns the difference of two numbers."""
+    return argument_a - argument_b
